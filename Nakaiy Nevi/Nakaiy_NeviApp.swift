11b13c05a7122236ec70d1cf609527713d2dbda0
Simon Shareef <simonshareef@gmail.com>
2023-05-10 21:10:11 +0500

    Initial Commit

---
+++ Nakaiy Nevi/Nakaiy_NeviApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nakaiy_NeviApp.swift
+//  Nakaiy Nevi
+//
+//  Created by Simon Shareef on 10/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct Nakaiy_NeviApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
